a6e08ade971d8401dae1fe5022fd26790cd024f7
Poppy_Bu <Poppy_Bu@asus.com>
2017-05-31 14:10:15 +0800

    about Android Broadcast Pratice,and finish the function of force online

---
+++ app/src/main/java/com/example/broadcastbestpractice/mylogin.java
@@ -0,0 +1,47 @@
+package com.example.broadcastbestpractice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class mylogin extends BaseActivity {
+
+    private EditText accountEdit;
+
+    private EditText passwordEdit;
+
+    private Button login;
+
+    private CheckBox rememberPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mylogin);
+       // pref = PreferenceManager.getDefaultSharedPreferences(this);
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                // 如果账号是admin且密码是123456，就认为登录成功
+                if (account.equals("admin") && password.equals("123456")) {
+                    Intent intent=new Intent("com.example.broadcastbestpractice.MyLogin");
+                    sendBroadcast(intent);
+                } else {
+                    Toast.makeText(mylogin.this, "account or password is invalid",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
